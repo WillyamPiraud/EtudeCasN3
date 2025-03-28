38a5dfe4228ff519168fa2242902aa912dbd8eec
WillyamPiraud <98462875+WillyamPiraud@users.noreply.github.com>
2025-03-28 18:07:50 +0100

    Changed Commentary

--- api/articles/articles.services.js
+++ api/articles/articles.services.js
@@ -1,58 +1,5 @@
 const Article = require("./articles.schema");
 const mongoose = require("mongoose");
-/*
-async function createArticle(data) {
-  try {
-    const now = new Date();
-    data.createdAt = now;
-    const article = await Article.create(data);
-    return article;
-  } catch (error) {
-    console.error("❌ Erreur lors de la création de l'article:", error);
-    throw error;
-  }
-}
-
-async function updateArticle(id, data) {
-  try {
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      throw new Error("ID invalide");
-    }
-
-    const updatedArticle = await Article.findByIdAndUpdate(id, data, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!updatedArticle) {
-      throw new Error("Article non trouvé");
-    }
-
-    return updatedArticle;
-  } catch (error) {
-    console.error("Erreur lors de la mise à jour de l'article:", error.message);
-    throw error;
-  }
-}
-
-async function deleteArticle(articleId) {
-  return await Article.findByIdAndDelete(articleId);
-}
-
-async function getArticlesByUser(userId) {
-  return await Article.find({ user: userId }).populate({
-    path: "user",
-    select: "-password",
-  });
-}
-
-module.exports = {
-  createArticle,
-  updateArticle,
-  deleteArticle,
-  getArticlesByUser,
-};
-*/
 
 class ArticleService {
   createArticle(data) {
@@ -64,47 +11,9 @@ class ArticleService {
       throw error;
     }
   }
-
-  /* 
-  async updateArticle(id, data) {
-    console.log("🔄 Données avant update :", data);
-    return await Article.findByIdAndUpdate(id, data, {
-      new: true,
-    });
-  }*/
   update(id, data) {
     return Article.findByIdAndUpdate(id, data, { new: true });
   }
-
-  /*async updateArticle(id, data) {
-    try {
-      if (!mongoose.Types.ObjectId.isValid(id)) {
-        throw new Error("ID invalide");
-      }
-
-      console.log("🔄 Mise à jour de l'article avec ID:", id);
-      console.log("📜 Données envoyées pour mise à jour:", data);
-
-      const updatedArticle = await Article.findByIdAndUpdate(id, data, {
-        new: true,
-        runValidators: true,
-      });
-
-      if (!updatedArticle) {
-        console.error("❌ Mise à jour échouée, article introuvable.");
-        return { error: "Article introuvable" }; // Retourner un message d'erreur plus précis
-      }
-
-      console.log("✅ Article après mise à jour:", updatedArticle);
-      return updatedArticle;
-    } catch (error) {
-      console.error(
-        "❌ Erreur lors de la mise à jour de l'article:",
-        error.message
-      );
-      throw error; // Vous pouvez ajouter un message d'erreur plus descriptif ici
-    }
-  }*/
 
   deleteArticle(articleId) {
     try {
